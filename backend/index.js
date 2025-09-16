8f5f971867fbd6a139da929d78800f6311ecbc48
Prashant Garg <prashanttgarg366@gmail.com>
2025-09-16 22:01:23 +0530

    backend setup and writing controllers, middlewares and routes

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+
+import routes from "./routes/index.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//app.use(cors({origin:[""]}))
+app.use(cors('*'));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api-v1", routes);
+
+app.use((req, res,next) => {
+  res.status(404).json({
+    status: "404 Not found",
+    message: "Route not found",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
