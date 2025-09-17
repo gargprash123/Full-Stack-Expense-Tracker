027bba07e2176ce5d9838108cb1840d57faa5c4f
Prashant Garg <146079986+gargprash123@users.noreply.github.com>
2025-09-17 22:39:36 +0530

    Update api.js

--- frontend/src/libs/api.js
+++ frontend/src/libs/api.js
@@ -3,7 +3,7 @@ import useStore from '../store';
 
 // Set your backend URL in a .env file at the root of /frontend
 // VITE_API_URL=http://localhost:8000/api-v1
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api-v1';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api-v1';
 
 const api = axios.create({
   baseURL: API_URL,
