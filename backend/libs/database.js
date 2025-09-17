b58049bfc378a73e2b33c2faf20e2bd773836fda
Prashant Garg <prashanttgarg366@gmail.com>
2025-09-17 21:59:21 +0530

    Debug: Log database URI

--- backend/libs/database.js
+++ backend/libs/database.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pg;
-
+console.log("DATABASE_URI is:", process.env.DATABASE_URI);
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URI,
   ssl: {
